1416b88489682e2ca2f8deee9b3a53ab11c4344f
shankarkhatri <khatrishankar101@gmail.com>
2026-03-29 22:28:43 +0530

    firstReactApp

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import '../css/index.css'
+import Form from './Form'
+function App(){
+    return (
+        <>
+        <Form/>
+        </>
+    )
+}
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import '../css/index.css'
+import styles from '../css/Form.module.css'
+function Form(){
+return(
+    <>
+    <div id={styles.formDiv}>
+        <h1>Hello</h1>
+    </div>
+    </>
+)
+}
+export default Form;
